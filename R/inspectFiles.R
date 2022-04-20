9cad555e5abac5b72fbd4fad72ebff822781b587
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2022-04-20 17:42:11 +0200

    first version

--- R/inspectFiles.R
+++ R/inspectFiles.R
@@ -9,16 +9,41 @@ inspectFiles = function(){
   mon = menu(choices = c("FIBION", "Scriin", "ActiGraph", "Other"),
              title = "\nWhat monitor have you used to record steps?")
   if (mon == 1) {
+    id_col = c()
     mon = "FIBION"
     format = "csv"
+    timestamp_col = "local"
+    timestamp_format = "iso"
+    epoch = 900
+    steps = "activity.steps.count"
+    steps_sporadic = "activity.steps2.count"
+    sit2stand = "activity.sit2stand.count"
+    resample = T
   } else if (mon == 2) {
+    id_col = 1
     mon = "Scriin"
     format = "txt"
+    timestamp_col = 2
+    timestamp_format = "%Y-%m-%d %H:%M:%S"
+    epoch = 60
+    steps = 3
+    steps_sporadic = c()
+    sit2stand = c()
+    resample = T
   } else if (mon == 3) {
+    id_col = c()
     mon = "ActiGraph"
     format = menu(choices = c("agd", "csv"),
                   title = "\nWhat is the format of your files?")
     if (format == 1) format = "agd" else if (format == 2) format = "csv"
+    if (format == "agd") stop("agd files cannot be read at the moment")
+    timestamp_col = "timestamp"
+    timestamp_format = c()
+    epoch = "epochlengthinseconds"
+    steps = "steps"
+    steps_sporadic = c()
+    sit2stand = c()
+    resample = F
   } else if (mon == 4) {
     stop("Function to analyze data from other monitors is under development")
   }
@@ -29,6 +54,7 @@ inspectFiles = function(){
   datadir = utils::choose.dir(default = getwd(),
                               caption = paste("Select the directory containing the", mon, format, "files"))
   files = dir(datadir, pattern = paste0(".", format))
+  files_full = dir(datadir, pattern = paste0(".", format), full.names = TRUE)
   nFiles = length(files)
   cat(paste("\nThere are", nFiles, mon, "files in this folder...\n"))
   print(files)
@@ -42,11 +68,18 @@ inspectFiles = function(){
   outputdir = utils::choose.dir(default = getwd(),
                               caption = "Select the output directory")
 
+  if (!dir.exists(file.path(outputdir, mon))) dir.create(file.path(outputdir, mon))
+  outputdir = file.path(outputdir, mon)
+
   # save directories
   metadatadir = file.path(outputdir, "metadata/")
   if (!dir.exists(metadatadir)) dir.create(metadatadir)
   save(datadir, outputdir, metadatadir,files, mon, format,
-       file = file.path(outputdir, "log_directories.RData"))
+       file = file.path(metadatadir, "metadata.RData"))
 
-  invisible(list(datadir, outputdir, metadatadir,files, mon, format))
+  invisible(list(datadir = datadir, outputdir = outputdir, metadatadir = metadatadir,
+                 files = files_full, mon = mon, format = format, id_col = id_col,
+                 timestamp_col = timestamp_col, timestamp_format = timestamp_format,
+                 epoch = epoch, steps = steps, steps_sporadic = steps_sporadic,
+                 sit2stand = sit2stand, resample = resample))
 }
--- R/step.metrics.R
+++ R/step.metrics.R
@@ -1,145 +1,136 @@
-roxygen2::roxygenise()
+#' step.metrics
+#'
+#' @return data frames with aggregated data
+#' @export
+#'
+#' @examples
+step.metrics = function(){
 
-step.metrics = function(datadir, outputdir="./", 
-                        timestamp_colname = "timestamp", 
-                        steps_colname = "steps",
-                        th.MOD=100, th.VIG=130, 
-                        includedaycrit = 10,
-                        exclude_pk30_0 = TRUE,
-                        exclude_pk60_0 = TRUE,
-                        time_format = c()){
-  
-  print("Calculating features per day")
-  
-  # timeformats to try
-  if(is.null(time_format)) {
-    tryFormats = c("%Y-%m-%d %H:%M:%OS",
-                  "%Y/%m/%d %H:%M:%OS",
-                  "%Y-%m-%d %H:%M",
-                  "%Y/%m/%d %H:%M",
-                  "%m/%d/%Y %H:%M",
-                  "%Y-%m-%d",
-                  "%Y/%m/%d")
-  }
-  
-  
-  # Functions
-  chartime2iso8601 = function(x,tz = "", time_format = c()){
-    if(is.null(time_format)) {
-      POStime = as.POSIXlt(as.numeric(as.POSIXlt(x,tz, tryFormats = tryFormats)), origin = "1970-01-01", tz)
-    } else if(!is.null(time_format)) {
-      POStime = as.POSIXlt(as.numeric(as.POSIXlt(x,tz, format = time_format)), origin = "1970-01-01", tz)
-    }
-    POStimeISO = strftime(POStime,format="%Y-%m-%dT%H:%M:%S%z")
-    return(POStimeISO)
-  }
-  
-  #Names of the data files
-  files = dir(datadir, pattern = "*.csv")
-  time = timestamp_colname
-  steps = steps_colname
-  
-  #Loop through the files
+  # get directories and files to process ----
+  metadata = inspectFiles()
+  list2env(metadata, envir = environment())
+
+  # get thresholds ---
+  th.MOD = menu(c("100 steps per minute", "other"), title = "\n\nWhat cadence threshold do you want to use to classify Moderate Intensity PA?")
+  if (th.MOD == 1) th.MOD = 100
+  if (th.MOD == 2) th.MOD = as.numeric(readline("\n\nPlease, write the threshold to use (in steps per minute):"))
+
+  th.VIG = menu(c("130 steps per minute", "other"), title = "\n\nWhat cadence threshold do you want to use to classify Vigorous Intensity PA?")
+  if (th.VIG == 1) th.VIG = 130
+  if (th.VIG == 2) th.VIG = as.numeric(readline("\n\nPlease, write the threshold to use (in steps per minute):"))
+
+
+  cat("\n\nCalculating features per day...\n")
+  pb = txtProgressBar(1, length(files), style = 3)
+
+  #Loop through the files ----
   for (i in 1:length(files)) {
-    S = read.csv(paste0(datadir, "/", files[i]))
-    t = chartime2iso8601(S$timestamp, time_format = time_format)
-    # t = unclass(as.POSIXlt(t, format="%Y-%m-%dT%H:%M:%S%z"))
-    # mnightsi = which(t$sec==0 & t$min==0 & t$hour==0)
-    mnightsi = grep("00:00:00", t, fixed = T)
-    day = rep(NA, times=nrow(S))
-    d=1
-    #Loop through the rest of the days
-    for(j in 1:length(mnightsi)){
-      if (j < length(mnightsi)){
-        if (j == 1 & mnightsi[j] > 1){
-          day[1:mnightsi[j]] = d
-        } else if(j == 1 & mnightsi[j] == 1){
-          next()
-        } else {
-          day[mnightsi[j-1]:mnightsi[j]] = d
-        }
-        d=d+1
-      } else {
-        day[mnightsi[j-1]:mnightsi[j]] = d; d = d+1
-        day[mnightsi[j]:length(day)] = d
-      }
-    }
+    # read file
+    acc = readFile(file = files[i], mon = mon)
+
+    # aggregate per 60s epochs
+    if(epoch < 60) acc = agg60s(acc, epoch = epoch)
+
+    # resample if needed
+    if (isTRUE(resample)) acc = GenerationPepSteps::resample(acc, timestamp_col, timestamp_format, id_col = id_col, mon = mon)
+
+    # timestamp (if resampled, it is already in iso8601)
+    t = acc[, timestamp_col]
+    if (isFALSE(resample) & format != "iso") t = timestamp2iso8601(t, time_format = timestamp_format, tz = "Europe/Stockholm")
+
+    # detect midnights
+    midnights = detect_midnights(t)
+    list2env(midnights, envir = environment())
+
     #Loop through days to calculate variables
-    date = date_num = wday_num = record.min = stepsperday  = pk60min = N0s_pk60min = N0s_pk30min = pk30min = pk1min = NA
+    date = date_num = wday_num = day.min = stepsperday = SporadicStepsperday = sit2standperday = NA
+    pk60min = N0s_pk60min = N0s_pk30min = pk30min = pk1min = NA
     band_0 = band_1_19 = band_20_39 = band_40_59 = band_60_79 = band_80_99 = band_100_119 = band_120_higher = NA
     MPA = VPA = MVPA = NA
-    for (di in 1:length(unique(day))){
+    for (di in 1:length(unique(day))) {
       #Date
-      date[di] = strsplit(t[which(day==di)[1]], "T")[[1]][1]
-      if(is.null(time_format)) {
-        wday_num[di] = format(as.POSIXct(S[which(day==di)[1],1], tryFormats = timeFormats),"%u")  
-      } else if(!is.null(time_format)) {
-        wday_num[di] = format(as.POSIXct(S[which(day==di)[1],1], format = time_format),"%u")  
-      }
+      date[di] = strsplit(t[which(day == di)[1]], "T")[[1]][1]
+      wday_num[di] = suppressMessages(format(lubridate::as_datetime(t[which(day == di)[1]]), "%u"))
       #Duration of the day (number of minutes recorded)
-      record.min[di] = length(S[which(day==di),steps])
+      day.min[di] = length(which(day == di))
+
       #Steps/day
-      stepsperday[di] = sum(S[which(day==di),steps])
+      if(!is.null(steps_sporadic)) acc[, steps] = acc[, steps] + acc[, steps_sporadic]
+      stepsperday[di] = sum(acc[which(day == di), steps])
+
+      #Sporadic steps and sit2stand per day
+      if (!is.null(steps_sporadic)) SporadicStepsperday[di] = sum(acc[which(day == di), steps_sporadic])
+      if (!is.null(sit2stand)) sit2standperday[di] = sum(acc[which(day == di), sit2stand])
+
       #Peaks cadence
-      pk60min[di] = mean(sort(S[which(day==di),steps], decreasing = TRUE)[1:60])
-      N0s_pk60min[di] = length(which(sort(S[which(day==di),steps], decreasing = TRUE)[1:60]==0))
-      pk30min[di] = mean(sort(S[which(day==di),steps], decreasing = TRUE)[1:30])
-      N0s_pk30min[di] = length(which(sort(S[which(day==di),steps], decreasing = TRUE)[1:30]==0))
-      pk1min[di] = max(S[which(day==di),steps])
+      pk60min[di] = mean(sort(acc[which(day == di), steps], decreasing = TRUE)[1:60])
+      N0s_pk60min[di] = length(which(sort(acc[which(day == di), steps], decreasing = TRUE)[1:60] == 0))
+      pk30min[di] = mean(sort(acc[which(day == di), steps], decreasing = TRUE)[1:30])
+      N0s_pk30min[di] = length(which(sort(acc[which(day == di), steps], decreasing = TRUE)[1:30] == 0))
+      pk1min[di] = max(acc[which(day == di), steps])
+
       #Cadence band levels
-      band_0[di] = length(which(S[which(day==di),steps]==0))
-      band_1_19[di] = length(which(S[which(day==di),steps] < 20 & S[which(day==di),steps] > 0))
-      band_20_39[di] = length(which(S[which(day==di),steps] < 40 & S[which(day==di),steps] >= 20))
-      band_40_59[di] = length(which(S[which(day==di),steps] < 60 & S[which(day==di),steps] >= 40))
-      band_60_79[di] = length(which(S[which(day==di),steps] < 80 & S[which(day==di),steps] >= 60))
-      band_80_99[di] = length(which(S[which(day==di),steps] < 100 & S[which(day==di),steps] >= 80))
-      band_100_119[di] = length(which(S[which(day==di),steps] < 120 & S[which(day==di),steps] >= 100))
-      band_120_higher[di] = length(which(S[which(day==di),steps] >= 120))
+      band_0[di] = length(which(acc[which(day == di), steps] == 0))
+      band_1_19[di] = length(which(acc[which(day == di), steps] < 20 & acc[which(day == di), steps] > 0))
+      band_20_39[di] = length(which(acc[which(day == di), steps] < 40 & acc[which(day == di), steps] >= 20))
+      band_40_59[di] = length(which(acc[which(day == di), steps] < 60 & acc[which(day == di), steps] >= 40))
+      band_60_79[di] = length(which(acc[which(day == di), steps] < 80 & acc[which(day == di), steps] >= 60))
+      band_80_99[di] = length(which(acc[which(day == di), steps] < 100 & acc[which(day == di), steps] >= 80))
+      band_100_119[di] = length(which(acc[which(day == di), steps] < 120 & acc[which(day == di), steps] >= 100))
+      band_120_higher[di] = length(which(acc[which(day == di), steps] >= 120))
+
       #MPA, VPA, MVPA
-      MPA[di] = length(which(S[which(day==di),steps] < th.VIG & S[which(day==di),steps] >= th.MOD))
-      VPA[di] = length(which(S[which(day==di),steps] >= th.VIG))
-      MVPA[di] = length(which(S[which(day==di),steps] >= th.MOD))
+      MPA[di] = length(which(acc[which(day == di), steps] < th.VIG & acc[which(day == di), steps] >= th.MOD))
+      VPA[di] = length(which(acc[which(day == di), steps] >= th.VIG))
+      MVPA[di] = length(which(acc[which(day == di), steps] >= th.MOD))
     }
     ##OUTPUT PER DAY
     names.out = c("id","date","wday_num","dur_day_min","th_MOD","th_VIG",
-                  "stepsperday","CAD_pk60_spm","CAD_N0s_pk60_spm","CAD_pk30_spm", "CAD_N0s_pk30_spm","CAD_pk1_spm",
+                  "stepsperday", "SporadicStepsperday", "sit2standperday",
+                  "CAD_pk60_spm","CAD_N0s_pk60_spm","CAD_pk30_spm", "CAD_N0s_pk30_spm","CAD_pk1_spm",
                   "band_CAD_0_min","band_CAD_1-19_min","band_CAD_20-39_min","band_CAD_40-59_min",
                   "band_CAD_60-79_min","band_CAD_80-99_min","band_CAD_100-119_min","band_CAD_120+_min",
                   "MPA_min","VPA_min","MVPA_min")
-    
-    daily.out = data.frame(matrix(NA, length(unique(day[which(is.na(day)==FALSE)])), length(names.out)))
+
+    daily.out = data.frame(matrix(NA, length(unique(day[which(is.na(day) == FALSE)])), length(names.out)))
     colnames(daily.out) = names.out
-    
-    fi=1
-    daily.out[,fi] = files[i]; fi=fi+1
-    daily.out[,fi:(fi+1)] = cbind(date, as.numeric(wday_num)); fi=fi+2
-    daily.out[,fi] = record.min; fi=fi+1
-    daily.out[,fi:(fi+1)] = cbind(rep(th.MOD, times=nrow(daily.out)),rep(th.VIG, times=nrow(daily.out))); fi=fi+2
-    daily.out[,fi:ncol(daily.out)] = cbind(stepsperday, pk60min, N0s_pk60min, pk30min, N0s_pk30min, pk1min,
-                                           band_0, band_1_19, band_20_39, band_40_59, band_60_79,
-                                           band_80_99, band_100_119, band_120_higher,
-                                           MPA, VPA, MVPA)
+
+    fi = 1
+    daily.out[, fi] = basename(files[i]); fi = fi + 1
+    daily.out[, fi:(fi + 1)] = cbind(date, as.numeric(wday_num)); fi = fi + 2
+    daily.out[, fi] = day.min; fi = fi + 1
+    daily.out[, fi:(fi + 1)] = cbind(rep(th.MOD, times = nrow(daily.out)), rep(th.VIG, times = nrow(daily.out))); fi = fi + 2
+    daily.out[, fi:ncol(daily.out)] = cbind(stepsperday, SporadicStepsperday, sit2standperday,
+                                            pk60min, N0s_pk60min, pk30min, N0s_pk30min, pk1min,
+                                            band_0, band_1_19, band_20_39, band_40_59, band_60_79,
+                                            band_80_99, band_100_119, band_120_higher,
+                                            MPA, VPA, MVPA)
     # Create output directory
-    if(dir.exists(outputdir)==FALSE) {
+    if(dir.exists(outputdir) == FALSE) {
       dir.create(outputdir)
     }
-    if(dir.exists(paste0(outputdir,"/daySummary"))==FALSE) {
+    if(dir.exists(paste0(outputdir,"/daySummary")) == FALSE) {
       dir.create(paste0(outputdir,"/daySummary/"))
     }
-    write.csv(daily.out, file = paste0(outputdir,"/daySummary/",files[i],"_DaySum",".csv"), row.names = F)
-    print(i)
+    write.csv(daily.out, file = paste0(outputdir,"/daySummary/", basename(files[i]), "_DaySum", ".csv"), row.names = F)
+    setTxtProgressBar(pb, i)
   }
-  
+
   ################################################################################################################################
   #Calculate means per week plain and weighted
-  
-  print("Calculating means per week")
-  
-  files = dir(paste0(outputdir,"/daySummary"))
-  
+
+  cat("\n\n")
+  cat(paste0(rep('_', options()$width), collapse = ''))
+  cat("\n\nCalculating means per week...\n")
+
+  files = dir(paste0(outputdir, "/daySummary"), full.names = T)
+  pb = txtProgressBar(1, length(files), style = 3)
+
   names.out.2 = c("id","start_date", "valid_days","valid_days_WD", "valid_days_WE",
                   "th_MOD","th_VIG",
                   "stepsperday_pla","stepsperday_wei",
+                  "SporadicStepsperday_pla","SporadicStepsperday_wei",
+                  "sit2standperday_pla","sit2standperday_wei",
                   "CAD_pk60_spm_pla","CAD_pk60_spm_wei", "CAD_N0s_pk60_spm_pla", "CAD_N0s_pk60_spm_wei",
                   "CAD_pk30_spm_pla","CAD_pk30_spm_wei", "CAD_N0s_pk30_spm_pla", "CAD_N0s_pk30_spm_wei",
                   "CAD_pk1_spm_pla","CAD_pk1_spm_wei",
@@ -148,36 +139,26 @@ step.metrics = function(datadir, outputdir="./",
                   "band_CAD_60-79_min_pla","band_CAD_60-79_min_wei","band_CAD_80-99_min_pla","band_CAD_80-99_min_wei",
                   "band_CAD_100-119_min_pla","band_CAD_100-119_min_wei","band_CAD_120+_min_pla","band_CAD_120+_min_wei",
                   "MPA_min_pla","MPA_min_wei","VPA_min_pla","VPA_min_wei","MVPA_min_pla","MVPA_min_wei")
-  
-  output = data.frame(matrix(NA, length(files), length(names.out.2)))
+
+  output = data.frame(matrix(NA, nrow = length(files), ncol = length(names.out.2)))
   colnames(output) = names.out.2
   #Loop through files to calculate mean variables
-  for (i in 1:length(files)){
-    D = read.csv(paste0(outputdir,"/daySummary/", files[i]))
-    exclude = sum(D$dur_day_min < includedaycrit * 60)
-    if(exclude > 0) D = D[-which(D$dur_day_min < includedaycrit * 60),]
-    if(exclude_pk30_0 == TRUE){
-      zeroes = sum(D$CAD_N0s_pk30_spm > 0)
-      if(zeroes > 0) D = D[-which(D$CAD_N0s_pk30_spm > 0),]
+  for (i in 1:length(files)) {
+    D = read.csv(paste0(outputdir,"/daySummary/", basename(files[i])))
+    fi = 1                                                  #fi is the column of the new output data frame
+    output[i,fi] = files[i]; fi = fi + 1
+    output[i,fi] = D[1,"date"]; fi = fi + 1
+    output[i,fi] = nrow(D); fi = fi + 1
+    output[i,fi] = sum(D$wday_num < 6); fi = fi + 1
+    output[i,fi] = sum(D$wday_num >= 6); fi = fi + 1
+    output[i,fi:(fi + 1)] = c(th.MOD, th.VIG); fi = fi + 2
+
+    for (mi in 7:ncol(daily.out)) {
+      output[i,fi] = mean(D[,mi]); fi = fi + 1
+      output[i,fi] = ((mean(D[which(D$wday_num < 6), mi]) * 5) + (mean(D[which(D$wday_num >= 6), mi]) * 2)) / 7; fi = fi + 1
     }
-    if(exclude_pk60_0 == TRUE){
-      zeroes = sum(D$CAD_N0s_pk60_spm > 0)
-      if(zeroes > 0) D = D[-which(D$CAD_N0s_pk60_spm > 0),]
-    }
-    fi=1                                                  #fi is the column of the new output data frame
-    output[i,fi] = files[i]; fi=fi+1
-    output[i,fi] = D[1,"date"]; fi=fi+1
-    output[i,fi] = nrow(D); fi=fi+1
-    output[i,fi] = sum(D$wday_num < 6); fi=fi+1
-    output[i,fi] = sum(D$wday_num >= 6); fi=fi+1
-    output[i,fi:(fi+1)] = c(th.MOD, th.VIG); fi=fi+2
-    
-    for (mi in 7:ncol(daily.out)){
-      output[i,fi] = mean(D[,mi]); fi=fi+1
-      output[i,fi] = ((mean(D[which(D$wday_num < 6), mi]) * 5) + (mean(D[which(D$wday_num >= 6), mi]) * 2)) / 7; fi=fi+1
-    }
-    print(i)
+    setTxtProgressBar(pb, i)
   }
-  
+
   write.csv(output, file = paste0(outputdir,"/personSummary.csv"), row.names = FALSE)
 }
